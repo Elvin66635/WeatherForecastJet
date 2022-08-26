56f024f4ea3e5517708d225124c8387c2a121289
Dev_Elvin <elvin_66635@mail.ru>
2022-08-26 16:49:27 +0400

    anim. splash screen, navigate to main screen.

--- app/src/main/java/com/example/weatherforecastjet/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/example/weatherforecastjet/navigation/WeatherNavigation.kt
@@ -4,7 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.weatherforecastjet.screens.WeatherSplashScreen
+import com.example.weatherforecastjet.screens.MainScreen
+import com.example.weatherforecastjet.screens.main.splash.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
@@ -12,6 +13,9 @@ fun WeatherNavigation() {
     NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
         composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController = navController)
+        }
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
         }
     }
 }
---
+++ app/src/main/java/com/example/weatherforecastjet/screens/main/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.example.weatherforecastjet.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Text(text = "Main Screen")
+}
